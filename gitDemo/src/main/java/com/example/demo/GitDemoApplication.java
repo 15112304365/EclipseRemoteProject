1c2c2ae5e5cb7f3b0c4e6aa50baa60eddfcbc5d6
qwy <599068779@qq.com>
2019-05-14 11:25:52 +0800

    这是我第一个用git提交的spring boot项目

--- gitDemo/src/main/java/com/example/demo/GitDemoApplication.java
+++ gitDemo/src/main/java/com/example/demo/GitDemoApplication.java
@@ -8,6 +8,7 @@ public class GitDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
+		System.out.println("这是我第一个用git提交的spring boot项目");
 	}
 
 }
